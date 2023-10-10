fff8b22cc282e07d4937419b123d343d93418732
Kirito <102875535+KiritoGamesPlays@users.noreply.github.com>
2023-10-10 08:15:20 -0300

    Update Error.js

--- src/Error.js
+++ src/Error.js
@@ -4,7 +4,7 @@ const red = (message) => `\x1b[31m${message}\x1b[0m`;
 
 const yellow = (message) => `\x1b[33m${message}\x1b[0m`;
 
-const advertisement = `${yellow("[ WioDB ] => Information:")} ${green("Come here for help => https://discord.gg/BwyEkW4Qax")}`;
+const advertisement = `${yellow("[ WioDB-V2 ] => Information:")} ${green("Come here for help => https://discord.gg/BwyEkW4Qax")}`;
 
 
 
@@ -20,7 +20,7 @@ class DatabaseError extends Error {
     }
 
     get name() {
-        return yellow(`[ WioDB ] => ${this.constructor.name}`);
+        return yellow(`[ WioDB-V2 ] => ${this.constructor.name}`);
     }
 }
 
