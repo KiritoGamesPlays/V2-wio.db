a3cf1761be232d0bcf8d450537f90d8a1bfbd031
Kirito <102875535+KiritoGamesPlays@users.noreply.github.com>
2023-10-10 08:23:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const JsonDatabasev2 = require("./src/JsonProvider");
-const Databasev2Error = require("./src/Error");
+const DatabaseError = require("./src/Error");
 const YamlDatabasev2 = require("./src/YamlProvider");
 
 
@@ -10,5 +10,5 @@ const YamlDatabasev2 = require("./src/YamlProvider");
 module.exports = {
     JsonDatabasev2,
     YamlDatabasev2,
-    Databasev2Error
+    DatabaseError
 };
